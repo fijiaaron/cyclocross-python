f62a2e3ef0a701eb21f84dd0610a79e1be2eb669
Aaron Evans <aarone@one-shore.com>
2021-09-26 05:41:33 -0600

    wordpress create post API working on dev but not on stage or prod

--- wordpress/create_post.py
+++ wordpress/create_post.py
@@ -1,5 +1,40 @@
-from selenium import webdriver
+import os 
+import base64
+import json
+import requests
+from datetime import datetime
 
-class Foo:
+#url = "https://cyclocross.qazar.net/wp-json/wp/v2/posts"
+#url = "https://cxmstaging.wpengine.com/wp-json/wp/v2/posts"
+url = "https://www.cxmagazine.com/wp-json/wp/v2/posts"
 
-    def __
+user = "AaronEvansGuru"
+password = os.getenv("CX_APPLICATION_PASSWORD")
+
+#### GETTING 403 error but it works on my server
+
+
+competition = "New Zealand National Championships"
+date = "15 Aug 2021"
+venue = "Lower Hutt, Wellington"
+country = "New Zealand"
+category = "Men Elite"
+shortcode = "[table id=NewZealandNationalChampionships-MenElite-15Aug2021 /]"
+d = datetime.strptime(date, "%d %b %Y")
+year = str(d.year)
+
+title = f"{year} {competition} {category} cyclocross results"
+
+content = f'''On {date}, {competition} was hosted at {venue}, {country}. 
+Here are the results for {category}.
+{shortcode}
+'''.format(length='multi-line')
+
+post = {
+ 'title'    : title,
+ 'status'   : 'draft', 
+ 'content'  : content,
+}
+
+response = requests.post(url, auth=(user, password), json=post)
+print(response)
